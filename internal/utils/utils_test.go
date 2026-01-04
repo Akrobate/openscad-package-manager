3e06e7054be99282d726808ced0eb99558c60105
Akrobate <fedorov.artiom@gmail.com>
2026-01-04 20:22:00 +0100

    Increasing coverage

--- internal/utils/utils_test.go
+++ internal/utils/utils_test.go
@@ -1,6 +1,14 @@
 package utils
 
-import "testing"
+import (
+	"os"
+	"path/filepath"
+	"testing"
+	"time"
+
+	git "github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing/object"
+)
 
 func TestGetNameFromDependencySpecString(t *testing.T) {
 	result, _ := GetNameFromDependencySpecString("https://gitlab.com/openscad-modules/breadboard.git")
@@ -29,5 +37,65 @@ func TestGetURLFromDependencySpecString(t *testing.T) {
 			"GetRefFromDependencySpecString(\"https://gitlab.com/openscad-modules/breadboard.git#Test\") = %s, attendu https://gitlab.com/openscad-modules/breadboard",
 			result,
 		)
+	}
+}
+
+func TestOpenscadReplaceDependenciesPathes_TempDir(t *testing.T) {
+	dir := t.TempDir() // crée un dossier temporaire
+
+	scadFile := filepath.Join(dir, "a.scad")
+	os.WriteFile(scadFile, []byte("<from/path/file>"), 0644)
+
+	txtFile := filepath.Join(dir, "b.txt")
+	os.WriteFile(txtFile, []byte("ne pas toucher"), 0644)
+
+	OpenscadReplaceDependienciesPathes(dir, "from/path", "to/path")
+
+	data, _ := os.ReadFile(scadFile)
+	if string(data) != "<to/path/file>" {
+		t.Errorf("contenu incorrect: %s", data)
+	}
+
+	data, _ = os.ReadFile(txtFile)
+	if string(data) != "ne pas toucher" {
+		t.Errorf("fichier non .scad modifié")
+	}
+}
+
+func TestGetGitHeadShortCommit(t *testing.T) {
+	dir := t.TempDir()
+
+	// 1. Creating empty git
+	repo, err := git.PlainInit(dir, false)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// 2. Create file for commit
+	file := filepath.Join(dir, "test.txt")
+	os.WriteFile(file, []byte("hello"), 0644)
+
+	// 3. Add commit
+	w, _ := repo.Worktree()
+	w.Add("test.txt")
+	commit, err := w.Commit("initial commit", &git.CommitOptions{
+		Author: &object.Signature{
+			Name:  "Test",
+			Email: "test@example.com",
+			When:  time.Now(),
+		},
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	hash, err := GetGitHeadShortCommit(dir)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	expected := commit.String()[:7]
+	if hash != expected {
+		t.Errorf("hash incorrect: got %s, want %s", hash, expected)
 	}
 }
